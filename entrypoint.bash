1068e501587f16ded4696270cf6567e0a2c1869a
mfclabber <novichkovdmitriy04@gmail.com>
2024-05-09 00:17:44 +0000

    Update scripts

--- entrypoint.bash
+++ entrypoint.bash
@@ -6,9 +6,11 @@ echo "CC/CXX: $CC/$CXX"
 echo "CFLAGS: $CFLAGS"
 echo "CXXFLAGS: $CXXFLAGS"
 
-# source /opt/ros/humble/setup.bash
-. ~/ros2_humble/install/local_setup.bash
-# .install/setup.bash
+source /opt/ros/humble/setup.bash
+
+# . ~/ros2_humble/install/local_setup.bash
+
+. ~/axior_ws/install/setup.bash
 
 echo "ROS_DISTRO: $ROS_DISTRO"
 echo "ROS_ROOT: $ROS_ROOT"
